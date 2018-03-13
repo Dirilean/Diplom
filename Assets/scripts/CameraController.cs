7c23ca61a15cff5d623db189e2f23849425b098f
Diana <31008695+Dirilean@users.noreply.github.com>
2018-03-13 19:49:04 +0500

    настроена камера

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -4,32 +4,86 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+
+    public float damping = 5f;
+    public Vector2 offset = new Vector2(2f, 3f);
+    public bool faceLeft;
+    private int lastX;
     [SerializeField]
-    private float speed;
+    Character player;
 
-    [SerializeField]
-    private Transform target;
-
-    Vector3 position;
-
-    private void Start()
+    void Start()
     {
-        speed = 3.0F;      
-        position.z = -10.0F;
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(faceLeft);
+        faceLeft=false;
     }
 
-    private void Awake()
+    public void FindPlayer(bool playerFaceLeft)
     {
-        if (!target) target = FindObjectOfType<Character>().transform; //поставить в таргет игрока
+        lastX = Mathf.RoundToInt(player.transform.position.x);
+        if (playerFaceLeft)
+        {
+            transform.position = new Vector3(player.transform.position.x - offset.x, offset.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x + offset.x, offset.y, transform.position.z);
+        }
     }
 
-    private void Update()
+    void Update()
     {
-        Vector3 position = new Vector3(target.position.x+4,4);
-        position.z = -10.0F;
-       transform.position =position;
-        //Vector3.Lerp(new Vector3(transform.position.x, 3, 0), position,speed*Time.deltaTime);//lerp для плавного движения
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.transform.position.x);
+          //  if (currentX > lastX) faceLeft = false; else if (currentX < lastX) faceLeft = true;
+            lastX = Mathf.RoundToInt(player.transform.position.x);
+
+            Vector3 target;
+            if (faceLeft)
+            {
+                target = new Vector3(player.transform.position.x - offset.x, offset.y, transform.position.z);
+            }
+            else
+            {
+                target = new Vector3(player.transform.position.x + offset.x, offset.y, transform.position.z);
+            }
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
+            transform.position = currentPosition;
+        }
     }
-	
+
+
+    //_____________________________
+
+    //[SerializeField]
+    //private float speed;
+
+    //[SerializeField]
+    //private Transform target;
+
+    //Vector3 position;
+
+    //private void Start()
+    //{
+    //    speed = 3.0F;      
+    //    position.z = -10.0F;
+    //}
+
+    //private void Awake()
+    //{
+    //    if (!target) target = FindObjectOfType<Character>().transform; //поставить в таргет игрока
+    //}
+
+    //private void Update()
+    //{
+    //    Vector3 position = new Vector3(target.position.x+4,4);
+    //    position.z = -10.0F;
+    //   transform.position =position;
+    //    //Vector3.Lerp(new Vector3(transform.position.x, 3, 0), position,speed*Time.deltaTime);//lerp для плавного движения
+    //}
+
 
 }
