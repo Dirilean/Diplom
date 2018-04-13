c21a119d6aed4cc943ab3ccdb072dd72e5c11009
Diana <31008695+Dirilean@users.noreply.github.com>
2018-04-13 18:39:09 +0500

    переписана боевка у игрока
    ближний бой теперь производится с помощью луча

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -26,17 +26,21 @@ public class Character : Unit
     public bool isGrounded=false; //проверка, стоит ли на земле
     int MinFireColb=0;//минимальное хп, при котором прекращаются выстрелы
     bool CheckJump;
-    bool IsEnemy;
-    Vector3 napravlenie;
-    int ProcentFireInColb;
+    Vector3 napravlenie;//куда смотрит игрок
     float TimeToPlusLives;//время перезарядки конвертации жизни
-    float LastTimeToPlusLives;
+    float LastTimeToPlusLives;//последнее время конвертьации жизней
+    
 
     float timeDie;//время смерти
     float zaderzhka = 1;//сколько секунд после смерти нужно ждать чтобы воскреснуть
 
     float TimeShoot;//время выстрела
     float delayShooy=0.3F;//задержка при выстрелах
+
+    float TimeDoDamage;//время удара в ближнем бою
+    float delayDoDamage= 0.3F;//задержка при ударах
+    public int damagehit;//урон в ближнем бою
+    public float damagehitdistanse;//дальность ближнего боя
 
 
     private void Start()
@@ -81,20 +85,25 @@ public class Character : Unit
         if (lives <= 0) { Die(); }
 
         if (Input.GetButtonDown("Fire2")) Shoot();//выстрел
-        if (Input.GetButtonDown("Fire1"))//ближний бой
-        {
-            DoDamage(new Vector2(transform.position.x+(0.55F* (GetComponent<SpriteRenderer>().flipX ? -1F : 1F)),this.transform.position.y+0.45F),0.3F, 11, 15, false); //точка удара, радиус поражения, слой врага, урон, на всех?
-        }
+        if (Input.GetButtonDown("Fire1")) DoDamage();//ближний бой
         if (Input.GetButtonDown("Lives")&&(lives<=80)) ConvertToLives();//поменять огонь на жизни
     }
 
-
-    private void CheckGround()
+    private void CheckGround()//проверка стоит ли персонаж на земле
     {
         //круг вокруг нижней линии персонажа. если в него попадают колайдеры то массив заполняется ими
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1F);
-            isGrounded = colliders.Length > 1; //один колайдер всегда внутри (кол. персонажа)
+        isGrounded = colliders.Length > 1; //один колайдер всегда внутри (кол. персонажа)
+    }
 
+    private void DoDamage()//урон в ближнем бою
+    {
+        if (TimeDoDamage + delayDoDamage < Time.time)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0.5F), Vector2.right * napravlenie.x, 2F, 1 << 11);
+            hit.collider.gameObject.GetComponent<Monster>().lives -= damagehit;
+            TimeDoDamage = Time.time;
+        }
     }
 
     private void Shoot()//выстрелы
@@ -102,7 +111,7 @@ public class Character : Unit
         if ((FireColb> MinFireColb)&&(TimeShoot+delayShooy<Time.time))
         {
             Vector3 position = new Vector3(transform.position.x + (GetComponent<SpriteRenderer>().flipX ? -0.3F : 0.3F), transform.position.y + 0.7F);//место создания пули относительно персонажа
-            Fire cloneFire = Instantiate<Fire>(FirePrefab, position, FirePrefab.transform.rotation);//создание экземпляра огня(пули)
+            Fire cloneFire = Instantiate(FirePrefab, position, FirePrefab.transform.rotation);//создание экземпляра огня(пули)
             cloneFire.Napravlenie = cloneFire.transform.right * (GetComponent<SpriteRenderer>().flipX ? -0.3F : 0.3F);//задаем направление и скорость пули (?если  true : false)
             cloneFire.Parent = gameObject;//родителем пули является текущий объект
             FireColb--;
@@ -113,7 +122,6 @@ public class Character : Unit
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.GetComponent<FireSphere>())//собирание огоньков
         {
             Destroy(collision.gameObject);
@@ -121,58 +129,7 @@ public class Character : Unit
         }
     }
 
- 
-
-    // функция возвращает ближайший объект из массива, относительно указанной позиции
-    static GameObject NearTarget(Vector3 position, Collider2D[] array)
-    {
-        Collider2D current = null;
-        float dist = Mathf.Infinity;
-
-        foreach (Collider2D coll in array)
-        {
-            float curDist = Vector3.Distance(position, coll.transform.position);
-
-            if (curDist < dist)
-            {
-                current = coll;
-                dist = curDist;
-            }
-        }
-
-        return current.gameObject;
-    }
-
-    // point - точка контакта
-    // radius - радиус поражения
-    // layerMask - номер слоя, с которым будет взаимодействие
-    // damage - наносимый урон
-    // allTargets - должны-ли получить урон все цели, попавшие в зону поражения
-    public static void DoDamage(Vector2 point, float radius, int layerMask, int damage, bool allTargets)//ближний бой
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(point, radius, 1 << layerMask);
-        if (!allTargets)//по 1 тарегу
-        {
-            GameObject obj = NearTarget(point, colliders);
-            if (obj.GetComponent<Monster>())
-            {
-                obj.GetComponent <SpriteRenderer>().color=Color.red;
-                obj.GetComponent<Monster>().lives -= damage;
-            }
-            return;
-        }
-
-        foreach (Collider2D hit in colliders)//по всем
-        {
-            if (hit.GetComponent<Monster>())
-            {
-                hit.GetComponent<Monster>().lives -= damage;
-            }
-        }
-    }
-
-
-    private void ConvertToLives()
+    private void ConvertToLives()//конвертирование огня в жизнь
     {
         if ((FireColb >= 50)&&(Time.time>LastTimeToPlusLives+TimeToPlusLives))//в колбе достаточно огня и прошло время перезарядки
         {
@@ -182,7 +139,7 @@ public class Character : Unit
         }
     }
 
-    void Die()
+    void Die()//смерть персонажа
     {
         gameObject.GetComponent<SpriteRenderer>().color = Color.black;
         lives = 0;
@@ -194,7 +151,6 @@ public class Character : Unit
             lives = 100;
             FireColb = 0;
         }
-
     }
 
 }
