62c30e7181ca9747352c133787aa940c302bfb73
Diana <31008695+Dirilean@users.noreply.github.com>
2018-04-26 20:40:07 +0500

    сделано масштабирование заднего фона (неба)

---
+++ Assets/scripts/fon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fon : MonoBehaviour {
+
+
+    [SerializeField]
+    Camera MyCamera;
+    float scale;
+    // Use this for initialization
+    void Start ()
+    {
+        scale = MyCamera.pixelWidth / 1086.0F;
+        transform.localScale = new Vector3(scale, 1, 0);
+        transform.localPosition =new Vector3(0,-MyCamera.pixelHeight/100+1);
+    }
+	
+
+}
