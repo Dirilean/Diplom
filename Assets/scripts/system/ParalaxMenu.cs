d9096f5b25e8fcd47bd73289eb637d1d3ef7b85d
Diana <31008695+Dirilean@users.noreply.github.com>
2018-05-13 23:44:08 +0500

    оформлено главное меню

---
+++ Assets/scripts/system/ParalaxMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxMenu : MonoBehaviour {
+
+    Vector3 offset;
+    Vector3 lastPos;
+    public float speed;
+	// Update is called once per frame
+	void Update ()
+    {
+        offset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - lastPos;
+        lastPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.Translate(offset * speed * Time.deltaTime);
+       // transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
